4d92ba9f8bf41ff881f94f40f474065581286bee
TrocyukS96 <trotzuk.stanislav@gmail.com>
2021-03-07 19:07:36 +0300

    slider for testimonials

--- js/script.js
+++ js/script.js
@@ -3,4 +3,26 @@ $(document).ready(function() {
       $('.burger__menu, .header__novigation').toggleClass('active');
       $('body').toggleClass('lock');
    });
+   $('.slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:1,
+		autoplay:true,
+		speed:3000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
 });
